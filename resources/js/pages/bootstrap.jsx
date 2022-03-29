04631d7ab8c837d95c54d38c47a0e6fca1c4b569
nesa402610 <argyleferris@gmail.com>
2022-03-29 16:20:43 +0300

    Небольшие фиксы классов

--- resources/js/pages/bootstrap.jsx
+++ resources/js/pages/bootstrap.jsx
@@ -10,33 +10,33 @@ const Bootstrap = () => {
                              src="https://avatars.mds.yandex.net/get-images-cbir/1604084/OzG1ArnQS5xUgC9wONqBow7138/ocr"
                              alt="Изображение поняшки"/>
                     </div>
-                    <div className="col-4">
+                    <div className="col-sm-4">
                         <div className={'bg-secondary text-white text-center p-4 h-100'}>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam amet beatae blanditiis
                             dolorem est fugiat illum, incidunt laborum minus molestiae numquam omnis provident quae,
                             quis quo repellat soluta, vitae.
                         </div>
                     </div>
-                    <div className="col-4">
+                    <div className="col-sm-4">
                         <img className={'img-fluid'}
                              src="https://avatars.mds.yandex.net/get-images-cbir/1604084/OzG1ArnQS5xUgC9wONqBow7138/ocr"
                              alt="Изображение поняшки"/>
                     </div>
                 </div>
                 <div className="row py-2 border border-dark bg-danger">
-                    <div className="col-4 ">
+                    <div className="col-sm-4 ">
                         <div className={'bg-secondary text-white text-center p-4 h-100'}>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam amet beatae blanditiis
                             dolorem est fugiat illum, incidunt laborum minus molestiae numquam omnis provident quae,
                             quis quo repellat soluta, vitae.
                         </div>
                     </div>
-                    <div className="col-4">
+                    <div className="col-sm-4">
                         <img className={'img-fluid'}
                              src="https://avatars.mds.yandex.net/get-images-cbir/1604084/OzG1ArnQS5xUgC9wONqBow7138/ocr"
                              alt="Изображение поняшки"/>
                     </div>
-                    <div className="col-4">
+                    <div className="col-sm-4">
                         <div className={'bg-secondary text-white text-center p-4 h-100'}>
                             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad aliquam amet beatae blanditiis
                             dolorem est fugiat illum, incidunt laborum minus molestiae numquam omnis provident quae,
