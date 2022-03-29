92ee23f44f598f50dd8117ef7bea541690cf5460
nesa402610 <argyleferris@gmail.com>
2022-03-29 16:22:16 +0300

    Сайдбар в отдельном + скрытие сайдара на макете

--- resources/js/index.jsx
+++ resources/js/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {Link, NavLink, Route, Routes} from 'react-router-dom';
+import React, {useEffect, useState} from 'react';
+import {Route, Routes, useLocation} from 'react-router-dom';
 import First_veb from "./pages/First_veb";
 import My_page from "./pages/my_page";
 import Traffic_light from "./pages/traffic_light";
@@ -8,31 +8,34 @@ import Positions from "./pages/positions";
 import Pxremem from "./pages/pxremem";
 import Flex from "./pages/flex";
 import Bootstrap from "./pages/bootstrap";
+import Aside_nav from "./components/aside_nav";
+import Bootstra2 from "./pages/bootstra-2";
+import BootstrapMaket from "./pages/bootstrap-maket";
 
 const Index = () => {
+    const [sidebar, setSidebar] = useState(true);
+    const location = useLocation()
+
+    useEffect(() => {
+        if (location.pathname === '/bootstrap-maket') {
+            setSidebar(false);
+        } else setSidebar(true)
+    }, [location]);
     return (
-        <div className={'container-fluid d-flex'}>
-            <aside className={'col-2 d-flex flex-column'} style={{gap: '.3rem'}}>
-                <NavLink className={'bg-light p-2'} to={'/'}>Первый вебинар</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/my_page'}>Второй вебинар</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/traffic_light'}>Светофор</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/flex'}>Flex</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/positions'}>Позиции</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/pxremem'}>Единицы измерения</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/contacts'}>Контакты</NavLink>
-                <NavLink className={'bg-light p-2'} to={'/bootstrap'}>Bootstrap</NavLink>
-                <NavLink className={'bg-light p-2 '} to={'/bootstrap'}>Кофемашина</NavLink>
-            </aside>
-            <div className="col">
+        <div className={`container-fluid px-0 ${sidebar ? ' d-flex' : ''}`}>
+            {sidebar ? <Aside_nav/> : ''}
+            <div>
                 <Routes>
                     <Route path={'/'} element={<First_veb/>}/>
                     <Route path={'/my_page'} element={<My_page/>}/>
                     <Route path={'/traffic_light'} element={<Traffic_light/>}/>
-                    <Route path={'positions'} element={<Positions/>} />
-                    <Route path={'flex'} element={<Flex/>} />
-                    <Route path={'pxremem'} element={<Pxremem/>} />
-                    <Route path={'contacts'} element={<Contacts/>} />
-                    <Route path={'bootstrap'} element={<Bootstrap/>} />
+                    <Route path={'positions'} element={<Positions/>}/>
+                    <Route path={'flex'} element={<Flex/>}/>
+                    <Route path={'pxremem'} element={<Pxremem/>}/>
+                    <Route path={'contacts'} element={<Contacts/>}/>
+                    <Route path={'bootstrap'} element={<Bootstrap/>}/>
+                    <Route path={'bootstrap2'} element={<Bootstra2/>}/>
+                    <Route path={'bootstrap-maket'} element={<BootstrapMaket/>}/>
                 </Routes>
             </div>
         </div>
