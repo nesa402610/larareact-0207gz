f6ef7457c525b5a0fb20119e4e395fc362480b97
nesa402610 <argyleferris@gmail.com>
2022-03-29 16:20:22 +0300

    В отдельный компонент сайдбар

---
+++ resources/js/components/aside_nav.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {NavLink} from "react-router-dom";
+
+const AsideNav = () => {
+    return (
+        <aside className={'col-sm-auto d-flex flex-column'}
+               style={{gap: '.3rem'}}>
+            <NavLink className={'bg-light p-2'}
+                     to={'/'}>Первый вебинар</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/my_page'}>Второй вебинар</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/traffic_light'}>Светофор</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/flex'}>Flex</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/positions'}>Позиции</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/pxremem'}>Единицы измерения</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/contacts'}>Контакты</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/bootstrap'}>Bootstrap</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/bootstrap2'}>Bootstrap 2</NavLink>
+            <NavLink className={'bg-light p-2'}
+                     to={'/bootstrap-maket'}>Bootstrap - верстка</NavLink>
+            <NavLink className={'bg-light p-2 '}
+                     to={'/cofe_ex_machina'}>Кофемашина</NavLink>
+
+        </aside>
+    );
+};
+
+export default AsideNav;
