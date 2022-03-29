f045b87a8dc52f6ef13900553e5e5511ccbb98e8
nesa402610 <argyleferris@gmail.com>
2022-03-29 16:23:19 +0300

    Второй бутстрап по вебинару

---
+++ resources/js/pages/bootstra-2.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Bootstra2 = () => {
+    return (
+        <div className={'container'}>
+            <div className="row">
+                <div className="col-md-2">
+                    Колонка 1
+                </div>
+                <div className="col-sm-auto">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur expedita fugit natus quod.
+                </div>
+                <div className="col-md-2">
+                    Колонка 2
+                </div>
+                <ul className={'list-inline'}>
+                    <li className={'list-inline-item'}>slaam</li>
+                    <li className={'list-inline-item'}>salam</li>
+                    <li className={'list-inline-item'}>salam</li>
+                </ul>
+
+            </div>
+        </div>
+    );
+};
+
+export default Bootstra2;
