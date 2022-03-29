57dc375d48d6af5c589def06e185d2a0b5b26c21
nesa402610 <argyleferris@gmail.com>
2022-03-29 16:22:26 +0300

    На будущее создан

---
+++ resources/js/pages/cofe_ex_machina.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CofeExMachina = () => {
+    return (
+        <div className={'container-fluid p-2'}>
+
+        </div>
+    );
+};
+
+export default CofeExMachina;
