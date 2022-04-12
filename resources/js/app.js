8072abd49362cf1e1059e07366361fb41274cb3b
nesa402610 <argyleferris@gmail.com>
2022-04-12 13:38:46 +0300

    Фикс

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import index from "./Index";
+import index from "./index";
 import ReactDOM from "react-dom";
 import {BrowserRouter} from "react-router-dom";
 
